5bbe76c16e382b157b09788bfb53867b956b8db6
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-11-16 17:22:28 +0000

    Adds ansible and containers

--- container/run_detection.Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM autoreduction/base
-
-USER isisautoreduce
-RUN python3 -m pip install --user autoreduce_run_detection.run_detection
-
-CMD python3 -m autoreduce_run_detection.run_detection
--- setup.py
+++ setup.py
@@ -16,11 +16,12 @@ setup_requires = [
     'requests',
 ]
 
-setup(
-    name='autoreduce-run-detection',
-    version='22.0.0.dev0',
-    description='A service to discover and ingest new files produced by the ISIS neutron and muon facility',
-    author='ISIS Autoreduction Team',
-    url='https://github.com/ISISScientificComputing/autoreduce-run-detection',
-    install_requires=setup_requires,
-)
+setup(name='autoreduce-run-detection',
+      version='22.0.0.dev0',
+      description='A service to discover and ingest new files produced by the ISIS neutron and muon facility',
+      author='ISIS Autoreduction Team',
+      url='https://github.com/ISISScientificComputing/autoreduce-run-detection',
+      install_requires=setup_requires,
+      classifiers=[
+          "Programming Language :: Python :: 3 :: Only",
+      ])
