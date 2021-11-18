912a44c791b218412aaf6000b5985d3d504fb976
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-11-18 13:37:47 +0000

    Actually include package data in the package!

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 Setup for the project
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup_requires = [
     'autoreduce_utils>=22.0.0.dev7',
@@ -17,11 +17,12 @@ setup_requires = [
 ]
 
 setup(name='autoreduce-run-detection',
-      version='22.0.0.dev1',
+      version='22.0.0.dev3',
       description='A service to discover and ingest new files produced by the ISIS neutron and muon facility',
       author='ISIS Autoreduction Team',
       url='https://github.com/ISISScientificComputing/autoreduce-run-detection',
       install_requires=setup_requires,
+      packages=find_packages(),
       classifiers=[
           "Programming Language :: Python :: 3 :: Only",
       ],
